f5003ba3bcf1fccdb37f1679dfc2674e1216acc5
Ratandeep Sharma <47265795+Ratandeep18@users.noreply.github.com>
2022-07-23 15:42:44 -0400

    Pub Failure logs

---
+++ Powershell/greenarrow_pubfailure_logs.ps1
@@ -0,0 +1,62 @@
+#Path set for KA logs moved
+$logl = "C:\scripts\greenarrow_pub_failure_logs\" + "ka_logs_" + (Get-date).ToString("yyyy-MM-dd_HH-mm-ss") + ".txt";
+
+# test if the directory exist if not it'll create it
+$log_path = "C:\scripts\greenarrow_pub_failure_logs\";
+if (-not (Test-Path $log_path)) {
+    mkdir $log_path
+}
+
+#create catalog file and temp file
+$log_file = "C:\scripts\greenarrow_pub_failure_logs\temp_logs_name.txt";
+if (-not (Test-Path $log_file)) {
+    New-Item -Path "C:\scripts\greenarrow_pub_failure_logs\" -Name "temp_logs_name.txt";
+}
+$temp_file = "C:\scripts\greenarrow_pub_failure_logs\temp_logs_name.temp";
+if (-not (Test-Path $temp_file)) {
+    New-Item -Path "C:\scripts\greenarrow_pub_failure_logs\" -Name "temp_logs_name.temp";
+}
+Set-Location 'C:\Shares\JB KA\ErrorFiles\PubFailure';
+cmd /c "dir /b" | Select-String -Pattern 'KA_' | Select-String -Pattern '.zip' | Out-File -FilePath "C:\scripts\greenarrow_pub_failure_logs\temp_logs_name.temp";
+
+#last logs of files in pub failure
+$ka_logs = Get-Content "C:\scripts\greenarrow_pub_failure_logs\temp_logs_name.txt"
+
+# start-transcript
+Start-Transcript -Path $logl;
+
+$file_count = (Get-ChildItem 'C:\Shares\JB KA\ErrorFiles\PubFailure').count
+
+if ($file_count -gt 0) {
+    #UNITED FILES
+    Get-ChildItem 'C:\Shares\JB KA\ErrorFiles\PubFailure' -Filter KA_United* | ForEach-Object {
+        for ($i = 0; $i -eq $ka_logs.Length; $i++) {
+            $name = $_.BaseName;
+            if (-not ($name -eq $ka_logs[$i])) {
+                Move-Item -Destination 'C:\Shares\FileRouteDirectories' -Force -Verbose;
+            }
+        }
+    }
+    #Jetblue FILES
+    Get-ChildItem 'C:\Shares\JB KA\ErrorFiles\PubFailure' -Filter KA_JetBlue* | ForEach-Object {
+        for ($i = 0; $i -eq $ka_logs.Length; $i++) {
+            $name = $_.BaseName;
+            if (-not ($name -eq $ka_logs[$i])) {
+                Move-Item -Destination 'C:\Shares\JB KA' -Force -Verbose;
+            }
+        }
+    }
+    #Spirit FILES
+    Get-ChildItem 'C:\Shares\JB KA\ErrorFiles\PubFailure' -Filter KA_Spirit* | ForEach-Object {
+        for ($i = 0; $i -eq $ka_logs.Length; $i++) {
+            $name = $_.BaseName;
+            if (-not ($name -eq $ka_logs[$i])) {
+                Move-Item -Destination 'C:\Shares\SpiritKA' -Force -Verbose;
+            }
+        }
+    }
+    Clear-Content "C:\scripts\greenarrow_pub_failure_logs\temp_logs_name.txt";
+    Get-Content "C:\scripts\greenarrow_pub_failure_logs\temp_logs_name.temp" | Set-Content -Path "C:\scripts\greenarrow_pub_failure_logs\temp_logs_name.txt";
+    Clear-Content "C:\scripts\greenarrow_pub_failure_logs\temp_logs_name.temp";    
+}
+Stop-Transcript;
